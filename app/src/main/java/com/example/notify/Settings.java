79588dddee9e5fee89deedfae197f544d83e05e5
ArjunCB28 <arjuncb28@gmail.com>
2019-07-24 23:10:01 -0300

    settings changes

--- app/src/main/java/com/example/notify/Settings.java
+++ app/src/main/java/com/example/notify/Settings.java
@@ -1,65 +1,49 @@
 package com.example.notify;
 
 
-import android.R.layout;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.content.Context;
-import android.os.Build;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
-import androidx.core.app.NotificationCompat;
 import androidx.fragment.app.Fragment;
 
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
+import android.widget.AdapterView;
 import android.widget.Spinner;
-import android.widget.TextView;
 
-import java.util.ArrayList;
-import java.util.List;
-
-
-/**
- * A simple {@link Fragment} subclass.
- */
 public class Settings extends Fragment {
     private static final String TAG = "Settings";
+    Spinner spinner;
+
 
     public Settings() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_settings, container, false);
-        createNotificationChannel();
         // Inflate the layout for this fragment
+        spinner = view.findViewById(R.id.alarm_before_spinner);
+        Log.d(TAG, "onCreateView: ");
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                Log.d(TAG, "onItemSelected: "+position);
+                Log.d(TAG, "id: "+id);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                Log.d(TAG, "onItemSelected: ");
+                Log.d(TAG, "id: ");
+            }
+        });
+
         return inflater.inflate(R.layout.fragment_settings, container, false);
     }
-
-
-
-        private void createNotificationChannel() {
-        // Create the NotificationChannel, but only on API 26+ because
-        // the NotificationChannel class is new and not in the support library
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            CharSequence name = getString(R.string.app_name);
-            String description = "description";
-            int importance = NotificationManager.IMPORTANCE_DEFAULT;
-            NotificationChannel channel = new NotificationChannel("", name, importance);
-            channel.setDescription(description);
-            // Register the channel with the system; you can't change the importance
-            // or other notification behaviors after this
-            NotificationManager notificationManager = getContext().getSystemService(NotificationManager.class);
-            notificationManager.createNotificationChannel(channel);
-        }
-    }
-
-
 
 }
